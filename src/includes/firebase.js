8857e5a76ebb72b9c350d9dcb524385a7fdadc2e
yrysNM <yrys.n3m@gmail.com>
2023-06-19 09:41:37 +0600

    feat: add firebase upload files song

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAh3r53AQkO9SuQbxJqhLJAp_RVeWwNut4',
@@ -14,7 +15,14 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig)
 const auth = firebase.auth()
 const db = firebase.firestore()
+const storage = firebase.storage();
+
 
 const userCollection = db.collection('users')
 
-export { auth, db, userCollection }
+export {
+  auth,
+  db,
+  userCollection,
+  storage
+}
