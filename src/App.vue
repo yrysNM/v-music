64d9d3bc71432d17fb64b19afc2e83504ff6d000
yrysNM <yrys.n3m@gmail.com>
2023-07-03 08:48:38 +0600

    feat: add player song with scrollbar

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,13 @@
 <template>
   <app-header></app-header>
-  <router-view></router-view>
 
-  <!-- Player -->
-  <div class="fixed bottom-0 left-0 bg-white px-4 py-2 w-full">
-    <!-- Track Info -->
-    <div class="text-center">
-      <span class="song-title font-bold">Song Title</span> by
-      <span class="song-artist">Artist</span>
-    </div>
-    <div class="flex flex-nowrap gap-4 items-center">
-      <!-- Play/Pause Button -->
-      <button type="button">
-        <i class="fa fa-play text-gray-500 text-xl"></i>
-      </button>
-      <!-- Current Position -->
-      <div class="player-currenttime">00:00</div>
-      <!-- Scrub Container  -->
-      <div class="w-full h-2 rounded bg-gray-200 relative cursor-pointer">
-        <!-- Player Ball -->
-        <span class="absolute -top-2.5 -ml-2.5 text-gray-800 text-lg" style="left: 50%">
-          <i class="fas fa-circle"></i>
-        </span>
-        <!-- Player Progress Bar-->
-        <span class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400" style="width: 50%"></span>
-      </div>
-      <!-- Duration -->
-      <div class="player-duration">03:06</div>
-    </div>
-  </div>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
+
+  <app-player />
 
   <auth></auth>
 </template>
@@ -39,12 +18,14 @@ import AppHeader from './components/Header.vue';
 import Auth from "./components/Auth.vue";
 import useUserStore from "./stores/user";
 import { auth } from './includes/firebase';
+import AppPlayer from "./components/Player.vue";
 
 export default {
   name: "App",
   components: {
     AppHeader,
-    Auth
+    Auth,
+    AppPlayer,
   },
   computed: {
     ...mapWritableState(useUserStore, ['userLoggedIn']),
@@ -57,4 +38,17 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.fade-entry-form {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all .5s linear;
+}
+
+.fade-leave-to {
+  transition: all .5s linear;
+  opacity: 0;
+}
+</style>
--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -6,10 +6,15 @@
             <span class="text-gray-500 text-sm">{{ song.display_name }}</span>
         </div>
         <div class="text-gray-600 text-lg">
-            <span class="comments">
-                <i class="fa fa-comments text-gray-600"></i>
-                {{ song.commnet_count }}
-            </span>
+            <router-link custom :to="{ name: 'song', params: { id: song.docID }, hash: '#comments' }" v-slot="{
+                navigate
+            }">
+
+                <span class="comments" @click="navigate">
+                    <i class="fa fa-comments text-gray-600"></i>
+                    {{ song.comment_count }}
+                </span>
+            </router-link>
         </div>
     </li>
 </template>
