74ebf10073e09db8ae23d7e541e541ab1e3bf53c
yrysNM <yrys.n3m@gmail.com>
2023-07-04 13:14:04 +0600

    feat: add i18n for languages

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from './router'
 
 import { auth } from './includes/firebase'
 import Icon from './directives/icon'
+import i18n from "./includes/i18n";
 
 import './assets/base.css'
 import './assets/main.css'
@@ -22,6 +23,7 @@ auth.onAuthStateChanged(() => {
         app.use(createPinia())
         app.use(router)
         app.use(VeeValidatePlugin, { foo: 100 })
+        app.use(i18n);
         app.directive("icon", Icon);
 
         app.mount('#app')
