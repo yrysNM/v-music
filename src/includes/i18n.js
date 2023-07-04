fdc3f247188edd8000d7186bc90120f87e871c27
yrysNM <yrys.n3m@gmail.com>
2023-07-04 17:41:00 +0600

    fix: add config i18n for vite

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,10 +1,27 @@
- import { createI18n } from "vue-i18n";
-import en from "../locales/en.json"; 
+import { createI18n } from "vue-i18n";
+import en from "../locales/en.json";
+import kz from "../locales/kz.json";
 
- export  default createI18n({
-    locale: "en",
-    fallbackLocale: "en",
-    messages: {
-      en
-    }
- })
+export default createI18n({
+   locale: "kz",
+   fallbackLocale: "en",
+   messages: {
+      en,
+      kz
+   },
+   numberFormats: {
+      en: {
+
+         currency: {
+            style: "currency",
+            currency: "USD"
+         }
+      },
+      kz: {
+         currency: {
+            style: "currency",
+            currency: "T",
+         }
+      }
+   }
+})
