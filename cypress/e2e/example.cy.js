fdb26209831b098d303ded5346aaa0b623fa7881
yrysNM <yrys.n3m@gmail.com>
2023-07-18 09:57:21 +0600

    feat: add cupress

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,8 +1,9 @@
+import { describe, cv } from 'vitest';
 // https://on.cypress.io/api
 
 describe('My First Test', () => {
   it('visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cv.visit("/");
+    cv.contains("#header a:first-child", "Music");
   })
 })
